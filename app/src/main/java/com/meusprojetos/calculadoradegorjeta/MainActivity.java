3984e3682667a0207a72d6493369b54a9e962e82
Ana Luisa Dias <analudias@live.com>
2020-08-15 23:22:00 -0300

    implementando texto da barra de progresso

--- app/src/main/java/com/meusprojetos/calculadoradegorjeta/MainActivity.java
+++ app/src/main/java/com/meusprojetos/calculadoradegorjeta/MainActivity.java
@@ -3,12 +3,47 @@ package com.meusprojetos.calculadoradegorjeta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputEditText editTextValor;
+    private TextView textSeekBar;
+    private SeekBar seekBar;
+    private EditText editTextGorjeta;
+    private EditText editTextTotal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextValor = findViewById(R.id.editTextValor);
+        textSeekBar = findViewById(R.id.textSeekBar);
+        seekBar = findViewById(R.id.seekBar);
+        editTextGorjeta = findViewById(R.id.editTextGorjeta);
+        editTextTotal = findViewById(R.id.editTextTotal);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progress = progress*10;
+                textSeekBar.setText(progress + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
